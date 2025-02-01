24a228c17e583932e8a86a8d762754d8a4871e6d
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-01 15:37:04 +0000

    export createOption function for improved module compatibility

--- option.js
+++ option.js
@@ -1,8 +1,7 @@
-function createOption(user) {
+export function createOption(user) {
     const option = document.createElement("option");
     option.value = user.name;
     option.textContent = user.name;
     return option;
 }
 
-export default createOption;
