f76fe899cf53031643b4e82475202d7e92a0d6fd
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-01 15:30:01 +0000

    Update renderOptions to use users_data for option creation

--- renderOptions.js
+++ renderOptions.js
@@ -1,8 +1,9 @@
-const selectEl = document.querySelector("select");
+const selectEl = document.querySelector("#select-users");
 import { createOption } from "./option.js";
+import users_data from './data.js'
 
-function renderOptions(users) {
-  const optionList = users.map(createOption);
+function renderOptions() {
+  const optionList = users_data.map(createOption);
   selectEl.append(...optionList);
 }
 
