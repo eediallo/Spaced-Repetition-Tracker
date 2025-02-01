0bdda59f3cc03acbede52df6283f28004c84cdc3
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-01 15:39:57 +0000

    Export users_data for module compatibility and update renderOptions to use named import

--- data.js
+++ data.js
@@ -1,9 +1,7 @@
-users_data = [
-  { name: "User 1", id : 1 },
-  { name: "User 2", id : 2 },
-  { name: "User 3", id : 3},
-  { name: "User 4", id : 4},
-  { name: "User 5", id : 5},
+export const users_data = [
+  { name: "User 1", id: 1 },
+  { name: "User 2", id: 2 },
+  { name: "User 3", id: 3 },
+  { name: "User 4", id: 4 },
+  { name: "User 5", id: 5 },
 ];
-
-export default users_data;
--- renderOptions.js
+++ renderOptions.js
@@ -1,10 +1,8 @@
 const selectEl = document.querySelector("#select-users");
+import { users_data } from "./data.js";
 import { createOption } from "./option.js";
-import users_data from './data.js'
 
 export function renderOptions() {
   const optionList = users_data.map(createOption);
   selectEl.append(...optionList);
 }
-
-
