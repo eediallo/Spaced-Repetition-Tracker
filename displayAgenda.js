98453cd2912476cf1396f9b971bbefac8c7bb937
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-03 12:54:59 +0000

    Add displayAgendaForSelectedUser function to render user agenda

---
+++ displayAgenda.js
@@ -0,0 +1,14 @@
+import { users_data } from "./data.js";
+const agendaEl = document.querySelector("#user-agenda-container"); // does not exist yet ==> nadika in charge
+
+export function displayAgendaForSelectedUser(e) {
+  const userText = e.target.value;
+  const user = users_data.find((user) => user.name === userText);
+  const { title, dates } = user.agenda;
+  agendaEl.innerHTML = ""; // Clear previous content
+  for (const date of dates) {
+    const dateEl = document.createElement("div");
+    dateEl.textContent = `${title}, ${date}`;
+    agendaEl.appendChild(dateEl);
+  }
+}
