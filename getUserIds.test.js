4b709f45abfedca8800094a8b8edb00eb995d2d6
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-03 21:18:21 +0000

    Add test to ensure getUserIds returns exactly 5 ids

--- getUserIds.test.js
+++ getUserIds.test.js
@@ -4,4 +4,8 @@ describe("getUserIds", () => {
   test("Should not be empty", () => {
     expect(getUserIds().length > 0).toBe(true);
   });
+
+  test("Should have exactly 5 ids", () => {
+    expect(getUserIds().length === 5).toBe(true);
+  });
 });
