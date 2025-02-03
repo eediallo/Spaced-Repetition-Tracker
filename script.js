bef93dd6b702a9a68c6ad65c51dd916e20045b86
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-03 12:54:12 +0000

    Add event listener for user selection to display agenda

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-import { renderOptions } from "./renderOptions.js";
+import { displayAgendaForSelectedUser } from "./displayAgenda.js";
+import { renderOptions, selectEl } from "./renderOptions.js";
 
 function main() {
   renderOptions();
+
+  selectEl.addEventListener("change", displayAgendaForSelectedUser);
 }
 
 window.onload = main;
