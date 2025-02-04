fdce69e25b92b99268ee20704ca5ca3870abe3f1
Anna Fedyna <annafedyna03@gmail.com>
2025-02-04 09:38:45 +0000

    change of date output format

--- calculateDate.js
+++ calculateDate.js
@@ -1,24 +1,36 @@
-function calculateDate() {
-    const todayDate = new Date();
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
 
-    const weekLater = new Date(todayDate);
-    const monthLater = new Date(todayDate);
-    const threeMonthsLater = new Date(todayDate);
-    const sixMonthsLater = new Date(todayDate);
-    const yearLater = new Date(todayDate);
+  let suffix = "th";
+  if (day === 1 || day === 21 || day === 31) suffix = "st";
+  if (day === 2 || day === 22) suffix = "nd";
+  if (day === 3 || day === 23) suffix = "rd";
 
-    weekLater.setDate(todayDate.getDate() + 7);
-    monthLater.setMonth(todayDate.getMonth() + 1);
-    threeMonthsLater.setMonth(todayDate.getMonth() + 3);
-    sixMonthsLater.setMonth(todayDate.getMonth() + 6);
-    yearLater.setFullYear(todayDate.getFullYear() + 1);
-
-    return [
-      weekLater.toLocaleDateString(),
-      monthLater.toLocaleDateString(),
-      threeMonthsLater.toLocaleDateString(),
-      sixMonthsLater.toLocaleDateString(),
-      yearLater.toLocaleDateString(),
-    ];
+  return `${day}${suffix} ${month} ${year}`;
 }
-    
+
+export function calculateDates(defaultDate) {
+  const todayDate = new Date(defaultDate);
+
+  const weekLater = new Date(todayDate);
+  const monthLater = new Date(todayDate);
+  const threeMonthsLater = new Date(todayDate);
+  const sixMonthsLater = new Date(todayDate);
+  const yearLater = new Date(todayDate);
+
+  weekLater.setDate(todayDate.getDate() + 7);
+  monthLater.setMonth(todayDate.getMonth() + 1);
+  threeMonthsLater.setMonth(todayDate.getMonth() + 3);
+  sixMonthsLater.setMonth(todayDate.getMonth() + 6);
+  yearLater.setFullYear(todayDate.getFullYear() + 1);
+
+  return [
+    formatDate(weekLater),
+    formatDate(monthLater),
+    formatDate(threeMonthsLater),
+    formatDate(sixMonthsLater),
+    formatDate(yearLater),
+  ];
+}
