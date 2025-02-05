b122df5e02eae3fe6304db84989269ce6031ce09
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-05 11:26:07 +0000

    feat: enhance topic addition with local storage support

--- addTopic.js
+++ addTopic.js
@@ -1,10 +1,11 @@
 import { displayAgendaForSelectedUser } from "./displayAgenda.js";
-import { calculateDates } from "./calculateDate.js"; 
+import { calculateDates } from "./calculateDate.js";
 import { users_data } from "./data.js";
-
+import { addData, getData } from "./storage.js";
+const form = document.querySelector("form");
 
 export function addNewTopic() {
-    const selectedUserName = document.getElementById("select-users").value;
+  const selectedUserName = document.getElementById("select-users").value;
   const topicName = document.getElementById("form-topic").value;
   const startDate = document.getElementById("form-date").value;
 
@@ -17,11 +18,13 @@ export function addNewTopic() {
     title: topicName,
     dates: calculateDates(startDate),
   };
-  console.log(newTopic);
-    
-  const user = users_data.find((user) => user.name === selectedUserName);
-  user.agenda.push(newTopic);
 
-  displayAgendaForSelectedUser();
+  const user = users_data.find((user) => user.name === selectedUserName) || [];
+  user.agenda.push(newTopic);
+  addData(user.id, user);
+
+  //add user data to local storage
+
+  ///displayAgendaForSelectedUser();
   form.reset();
 }
