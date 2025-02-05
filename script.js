6d871a4b6b1e063de028c5baf41c2f0478b0fa5b
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-05 17:31:17 +0000

    feat: refactor event handlers for form submission and user selection

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 import { renderOptions } from "./renderOptions.js";
 import { setDefaultDate } from "./defaultDate.js";
 const selectEl = document.querySelector("#select-users");
-const newTopicForm = document.querySelector('#form-add-topic');
-import {addNewTopic} from "./addTopic.js"
+const newTopicForm = document.querySelector("#form-add-topic");
+import { addNewTopic } from "./addTopic.js";
 import { displayAgendaForSelectedUser } from "./displayAgenda.js";
 
 function main() {
   renderOptions();
   setDefaultDate();
+  eventHandlers();
+}
+
+function eventHandlers() {
   selectEl.addEventListener("change", displayAgendaForSelectedUser);
-  newTopicForm.addEventListener("submit", (e) => {
-    e.preventDefault;
-    addNewTopic();
-  });
+  newTopicForm.addEventListener("submit", addNewTopic);
 }
 
 window.onload = main;
