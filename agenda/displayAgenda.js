ca7895c55fbc2a13a5d5faa4ebff52d123cbee18
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-05 18:52:40 +0000

    feat: add displayAgendaForSelectedUser function to display user-specific agenda

---
+++ agenda/displayAgenda.js
@@ -0,0 +1,25 @@
+import { getUserIds, getData } from "../storage.js";
+import { selectEl } from "../renderOptions.js";
+const agendaEl = document.querySelector("#user-agenda-container");
+import { createAgendaTitleElement } from "./createTitleEl.js";
+
+
+export function displayAgendaForSelectedUser() {
+  const userText = selectEl.value.toLowerCase();
+  const userIds = getUserIds();
+  let userHasAgenda = false;
+  agendaEl.innerHTML = ""; // Clear previous content before adding new elements
+  for (const id of userIds) {
+    const userData = getData(id);
+    if (userData && userData.name && userData.name.toLowerCase() === userText) {
+      userHasAgenda = true;
+      const titleEl = createAgendaTitleElement(userData);
+      agendaEl.appendChild(titleEl);
+      const dateElements = createAgendaDateElements(userData.agenda);
+      dateElements.forEach((dateEl) => agendaEl.appendChild(dateEl));
+    }
+  }
+  if (!userHasAgenda) {
+    agendaEl.textContent = "No agenda found for this User";
+  }
+}
