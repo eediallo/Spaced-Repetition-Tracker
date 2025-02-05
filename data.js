e61089ef4025c93bfd8f20d458b8c57fca98f00c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-05 12:23:21 +0000

    refac: convert user agenda structure from object to array format

--- data.js
+++ data.js
@@ -34,37 +34,43 @@ export const users_data = [
   {
     name: "User 3",
     id: 3,
-    agenda: {
-      title: "Variables in JS",
-      dates: [
-        " 26th July 2025",
-        " 19th August 2025",
-        " 19th October 2025",
-        " 19th January 2026",
-        " 19th July 2026",
-      ],
-    },
+    agenda: [
+      {
+        title: "Variables in JS",
+        dates: [
+          " 26th July 2025",
+          " 19th August 2025",
+          " 19th October 2025",
+          " 19th January 2026",
+          " 19th July 2026",
+        ],
+      },
+    ],
   },
   {
     name: "User 4",
     id: 4,
-    agenda: {
-      title: "Objects in JS",
-      dates: [
-        " 26th July 2025",
-        " 19th August 2025",
-        " 19th October 2025",
-        " 19th January 2026",
-        " 19th July 2026",
-      ],
-    },
+    agenda: [
+      {
+        title: "Objects in JS",
+        dates: [
+          " 26th July 2025",
+          " 19th August 2025",
+          " 19th October 2025",
+          " 19th January 2026",
+          " 19th July 2026",
+        ],
+      },
+    ],
   },
   {
     name: "User 5",
     id: 5,
-    agenda: {
-      title: "",
-      dates: [],
-    },
+    agenda: [
+      {
+        title: "",
+        dates: [],
+      },
+    ],
   },
 ];
