ccb21298cd4890def543b6fcaf63ce3c1cf33a87
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-06 15:29:30 +0000

    refac: simplify date calculations by consolidating date variables into an object

--- dates/calculateDates.js
+++ dates/calculateDates.js
@@ -1,47 +1,49 @@
 import { formatDate } from "./formateDate.js";
 
 export function calculateDates(defaultDate) {
-  let selectedDate = new Date(defaultDate);
-  let currentDate = new Date(
+  const selectedDate = new Date(defaultDate);
+  const currentDate = new Date(
     new Date().getFullYear(),
     new Date().getMonth(),
     new Date().getDate()
   );
 
-  let weekLater = new Date(selectedDate);
-  let monthLater = new Date(selectedDate);
-  let threeMonthsLater = new Date(selectedDate);
-  let sixMonthsLater = new Date(selectedDate);
-  let yearLater = new Date(selectedDate);
+  const dates = {
+    weekLater: new Date(selectedDate),
+    monthLater: new Date(selectedDate),
+    threeMonthsLater: new Date(selectedDate),
+    sixMonthsLater: new Date(selectedDate),
+    yearLater: new Date(selectedDate),
+    twoMonthLater: new Date(selectedDate),
+    fiveMonthLater: new Date(selectedDate),
+    elevenMonthLater: new Date(selectedDate),
+  };
 
-  let twoMonthLater = new Date(selectedDate);
-  let fiveMonthLater = new Date(selectedDate);
-  let elevenMonthLater = new Date(selectedDate);
+  dates.weekLater.setDate(selectedDate.getDate() + 7);
+  dates.monthLater.setMonth(selectedDate.getMonth() + 1);
+  dates.threeMonthsLater.setMonth(selectedDate.getMonth() + 3);
+  dates.sixMonthsLater.setMonth(selectedDate.getMonth() + 6);
+  dates.yearLater.setFullYear(selectedDate.getFullYear() + 1);
+  dates.twoMonthLater.setMonth(selectedDate.getMonth() + 2);
+  dates.fiveMonthLater.setMonth(selectedDate.getMonth() + 5);
+  dates.elevenMonthLater.setMonth(selectedDate.getMonth() + 11);
 
-  twoMonthLater.setMonth(selectedDate.getMonth() + 2);
-  fiveMonthLater.setMonth(selectedDate.getMonth() + 5);
-  elevenMonthLater.setMonth(selectedDate.getMonth() + 11);
-
-  weekLater.setDate(selectedDate.getDate() + 7);
-  monthLater.setMonth(selectedDate.getMonth() + 1);
-  threeMonthsLater.setMonth(selectedDate.getMonth() + 3);
-  sixMonthsLater.setMonth(selectedDate.getMonth() + 6);
-  yearLater.setFullYear(selectedDate.getFullYear() + 1);
-
-  const startFromPastDate = [
-    formatDate(monthLater),
-    formatDate(twoMonthLater),
-    formatDate(fiveMonthLater),
-    formatDate(elevenMonthLater),
+  const startFromPastDateDates = [
+    formatDate(dates.monthLater),
+    formatDate(dates.twoMonthLater),
+    formatDate(dates.fiveMonthLater),
+    formatDate(dates.elevenMonthLater),
   ];
 
-  const startFromselectedDate = [
-    formatDate(weekLater),
-    formatDate(monthLater),
-    formatDate(threeMonthsLater),
-    formatDate(sixMonthsLater),
-    formatDate(yearLater),
+  const startFromselectedDateDates = [
+    formatDate(dates.weekLater),
+    formatDate(dates.monthLater),
+    formatDate(dates.threeMonthsLater),
+    formatDate(dates.sixMonthsLater),
+    formatDate(dates.yearLater),
   ];
 
-  return selectedDate < currentDate ? startFromPastDate : startFromselectedDate;
+  return selectedDate < currentDate
+    ? startFromPastDateDates
+    : startFromselectedDateDates;
 }
