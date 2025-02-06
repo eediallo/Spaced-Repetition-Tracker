79d2da7473325e3a4375cded8ebcec97add7a202
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-06 15:47:26 +0000

    refac: replace direct DOM selection with imported userSelectEl for user list rendering

--- dropDownMenu/createUserList.js
+++ dropDownMenu/createUserList.js
@@ -1,8 +1,8 @@
-export const selectEl = document.querySelector("#select-users");
 import { USERS } from "../data/users.js";
 import { createOption } from "./createOption.js";
+import { userSelectEl } from "../queries.js";
 
 export function createAndRenderUserList() {
   const optionList = USERS.map(createOption);
-  selectEl.append(...optionList);
+  userSelectEl.append(...optionList);
 }
