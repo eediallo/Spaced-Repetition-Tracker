d0e4f0d2ed189f62437206ee6853abace54694f2
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-06 16:08:34 +0000

    refac: streamline topic addition by using alertMsg and inputDate for improved user feedback and clarity

--- agenda/addTopic.js
+++ agenda/addTopic.js
@@ -2,17 +2,15 @@ import { displayAgendaForSelectedUser } from "./displayAgenda.js";
 import { getFutureDates } from "../dates/getFutureDates.js";
 import { USERS } from "../data/users.js";
 import { addData, getData } from "../data/storage.js";
-import { userSelectEl, topicEl } from "../queries.js";
+import { userSelectEl, topicEl, inputDate } from "../queries.js";
+import { alertMsg } from "./alertMsg.js";
 
 export function addNewTopic() {
   const selectedUserName = userSelectEl.value;
-
   const topicName = topicEl.value;
-  const startDate = document.getElementById("form-date").value;
-
+  const startDate = inputDate.value;
   if (!selectedUserName || !topicName || !startDate) {
-    alert("Please fill in all fields.");
-    return;
+    alertMsg("Please fill in all fields.");
   }
 
   const newTopic = {
@@ -21,24 +19,18 @@ export function addNewTopic() {
   };
 
   let user = USERS.find((user) => user.name === selectedUserName);
-
-  if (user) {
-    const existingData = getData(user.id) || user;
-
-    // Check if the topic already exists
-    const topicExists = existingData.agenda.some(
-      (topic) => topic.title === topicName
-    );
-    if (topicExists) {
-      alert("This topic already exists for the selected user.");
-      return;
-    }
-
-    existingData.agenda.push(newTopic);
-    user = existingData;
-    addData(user.id, user);
+  if (!user) {
+    alertMsg("User not found.");
   }
-  displayAgendaForSelectedUser();
 
+  const existingData = getData(user.id) || user;
+  if (existingData.agenda.some((topic) => topic.title === topicName)) {
+    alertMsg("This topic already exists for the selected user.");
+  }
+
+  existingData.agenda.push(newTopic);
+  addData(user.id, existingData);
+
+  displayAgendaForSelectedUser();
   topicEl.value = "";
 }
