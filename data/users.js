c163e652a4a79f580bc58fc9627b79936692cc79
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-06 11:41:51 +0000

    refac: rename variable

---
+++ data/users.js
@@ -0,0 +1,52 @@
+export const USERS = [
+  {
+    name: "Elhadj",
+    id: 1,
+    agenda: [
+      {
+        title: "",
+        dates: [],
+      },
+    ],
+  },
+  {
+    name: "Nadika",
+    id: 2,
+    agenda: [
+      {
+        title: "",
+        dates: [],
+      },
+    ],
+  },
+  {
+    name: "Anna",
+    id: 3,
+    agenda: [
+      {
+        title: "",
+        dates: [],
+      },
+    ],
+  },
+  {
+    name: "Daniel",
+    id: 4,
+    agenda: [
+      {
+        title: "",
+        dates: [],
+      },
+    ],
+  },
+  {
+    name: "Sally",
+    id: 5,
+    agenda: [
+      {
+        title: "",
+        dates: [],
+      },
+    ],
+  },
+];
