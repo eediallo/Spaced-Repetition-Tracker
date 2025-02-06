42f8c2d4e272fc7c954f0745cdc7a2d77fc8181c
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-02-06 15:46:35 +0000

    feat: add DOM element selectors for user, topic, and date inputs

---
+++ queries.js
@@ -0,0 +1,7 @@
+const userSelectEl = document.querySelector("#select-users");
+
+const topicEl = document.querySelector("#form-topic");
+
+const inputDate = document.querySelector("#form-date");
+
+export { userSelectEl, topicEl, inputDate };
